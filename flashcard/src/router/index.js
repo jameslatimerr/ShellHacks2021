96a10ee90760e04d74033ca3ef84c75c32c9b40a
jameslatimerr <james.latimer@ucf.edu>
2021-09-25 18:34:17 -0400

    Added flashcards finally

--- flashcard/src/router/index.js
+++ flashcard/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import flashcard from '../components/flash.vue'
 
 const routes = [
   {
@@ -7,6 +8,7 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {path: '/', name: 'flashcard', component: flashcard},
   {
     path: '/about',
     name: 'About',
--- flashcard/src/views/Cards.vue
+++ flashcard/src/views/Cards.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="card">
-    <h1>This is a cards page</h1>
+  <div class="Card">
+    <flashcard definition = 'The ball of fire in the sky' word = 'Sun'></flashcard>
+    <flashcard definition = 'deez2' word = 'nuts2'></flashcard>
   </div>
 </template>
+<script>
+import flashcard from '@/components/flash.vue'
+export default {
+  name: 'flash',
+  components: {
+    flashcard
+  }
+}
+</script>
